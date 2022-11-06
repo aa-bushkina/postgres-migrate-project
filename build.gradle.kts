fa1428a2369eaa8db39f67f57a21c0dee523e624
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-06 17:23:23 +0300

    make build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "ru.vk"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.flywaydb:flyway-core:9.6.0")
+    implementation("org.postgresql:postgresql:42.5.0")
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("com.google.inject:guice:5.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
