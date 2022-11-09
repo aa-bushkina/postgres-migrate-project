e25018f4dd419388c0ea7adeb95aa81e7a012f28
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-09 22:38:24 +0300

    fix ON DELETE in orders

--- src/main/resources/db/V2022.11.06.17.44__create.sql
+++ src/main/resources/db/V2022.11.06.17.44__create.sql
@@ -1,14 +1,14 @@
 CREATE TABLE tables
 (
-    table_num   INT  NOT NULL,
+    table_num      INT NOT NULL,
     seats_quantity INT NOT NULL,
     CONSTRAINT tables_pk PRIMARY KEY (table_num)
 );
 
 CREATE TABLE orders
 (
-    order_num   SERIAL  NOT NULL,
-    table_num INT NOT NULL REFERENCES tables (table_num) ON UPDATE CASCADE ON DELETE CASCADE,
+    order_num SERIAL NOT NULL,
+    table_num INT    NOT NULL REFERENCES tables (table_num) ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT orders_pk PRIMARY KEY (order_num)
 );
 
@@ -21,20 +21,20 @@ CREATE TABLE sections
 
 CREATE TABLE menu
 (
-    id   SERIAL  NOT NULL,
-    dish_name VARCHAR NOT NULL,
-    weight INT NOT NULL,
-    price MONEY NOT NULL,
-    section_id INT NOT NULL REFERENCES sections (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    id         SERIAL  NOT NULL,
+    dish_name  VARCHAR NOT NULL,
+    weight     INT     NOT NULL,
+    price      MONEY   NOT NULL,
+    section_id INT     NOT NULL REFERENCES sections (id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT menu_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE order_details
 (
-    id   SERIAL  NOT NULL,
-    order_id INT NOT NULL REFERENCES orders (order_num) ON UPDATE CASCADE ON DELETE CASCADE,
-    dish_id INT NOT NULL REFERENCES menu (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    quantity INT NOT NULL,
+    id       SERIAL NOT NULL,
+    order_id INT    NOT NULL REFERENCES orders (order_num) ON UPDATE CASCADE ON DELETE CASCADE,
+    dish_id  INT    NOT NULL REFERENCES menu (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    quantity INT    NOT NULL,
     CONSTRAINT order_details_pk PRIMARY KEY (id)
 );
 
